74766591ac83036681445dfc7170095218a889fd
gengke <geng_ke@sina.com>
2021-05-31 09:52:03 +0800

    added Speciality Map Impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtesService extends CrudService<Specialty, Long> {
+}
